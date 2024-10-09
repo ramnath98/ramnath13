1db3ec68ca79307c15690ad100602f63b2a1ce3e
ramnath98 <dongareramnath55@gmail.com>
2024-10-09 07:44:42 -0400

    Update Test.java

--- javaproject01/src/javaproject01/Test.java
+++ javaproject01/src/javaproject01/Test.java
@@ -6,11 +6,15 @@ public class Test {
 	{
 		System.out.println("m1");
 	}
+	
+	public void m2()
+	{
+		System.out.println("m2");
+	}
 	public static void main(String[] args) 
 	{
 		Test t = new Test();
 		t.m1();
-		
 		
 	}
 
