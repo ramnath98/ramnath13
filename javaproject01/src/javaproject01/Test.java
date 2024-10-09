43231842098eb6b26888fd26e86620c6a21960f5
ramnath98 <dongareramnath55@gmail.com>
2024-10-09 07:59:37 -0400

    add m6

--- javaproject01/src/javaproject01/Test.java
+++ javaproject01/src/javaproject01/Test.java
@@ -20,6 +20,10 @@ public class Test {
 	{
 		System.out.println("m5");
 	}
+	public void m6()
+	{
+		System.out.println("m5");
+	}
 	public static void main(String[] args) 
 	{
 		Test t = new Test();
