3e5f8a21a56a16009fcfd80067026babb9d9c645
ramnath98 <dongareramnath55@gmail.com>
2024-10-09 07:45:44 -0400

    add m3 by Ramnath

--- javaproject01/src/javaproject01/Test.java
+++ javaproject01/src/javaproject01/Test.java
@@ -7,9 +7,9 @@ public class Test {
 		System.out.println("m1");
 	}
 	
-	public void m2()
+	public void m3()
 	{
-		System.out.println("m2");
+		System.out.println("m3");
 	}
 	public static void main(String[] args) 
 	{
