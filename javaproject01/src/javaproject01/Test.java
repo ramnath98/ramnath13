fd2383d683db9c384bedf3af31c77a7f3d058fc9
ramnath98 <dongareramnath55@gmail.com>
2024-10-09 17:14:20 -0400

    add m1

---
+++ javaproject01/src/javaproject01/Test.java
@@ -0,0 +1,17 @@
+package javaproject01;
+
+public class Test {
+	
+	public void m1()
+	{
+		System.out.println("m1");
+	}
+	public static void main(String[] args) 
+	{
+		Test t = new Test();
+		t.m1();
+		
+		
+	}
+
+}
